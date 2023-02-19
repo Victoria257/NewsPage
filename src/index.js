87084b0b294025123ac75af66ec77ae0b131a768
Viktoria <vik25@i.ua>
2023-02-19 20:36:08 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import './js/refs';
+import './js/local-storage';
+import './js/api';
+import './js/markup';
+import './js/popular';
+import './js/read-more';
+import './js/search';
+
+import './js/mobile-menu';
+import './js/dark_theme';
+
+import './js/news-filter/news-filter-btn';
+import './js/news-filter/news-filter-page';
+import './js/calendar';
+import './js/weather';
+import './js/pagination';
